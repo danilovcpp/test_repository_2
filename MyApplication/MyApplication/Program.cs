cf8cd2c66a0dc7e7aef2c71a04c30e2864f768ad
Alexander Danilov <alexander.danilov@indeed-id.com>
2024-12-18 21:25:14 +0600

    Добавил приветствие по имени

--- MyApplication/MyApplication/Program.cs
+++ MyApplication/MyApplication/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = Console.ReadLine();
+
+Console.WriteLine("Hello, " + name);
+
+Console.ReadLine();
